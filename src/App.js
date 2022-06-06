7789b6281cab56dfb2a9dbc2fc7cebbbe32a8373
adamalcantara <alcantaradevelopment@gmail.com>
2022-06-05 23:10:12 -0400

    Import added, page initial code added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 // import pages
+import LandingPage from "./Pages/LandingPage/LandingPage"
 
 function App() {
   return (
