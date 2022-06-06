0a59cf0605b2fbde90784b43c68ac7b025ba6e8d
adamalcantara <alcantaradevelopment@gmail.com>
2022-06-05 23:27:09 -0400

    Added Spotify icon

--- src/Components/Social/Social.js
+++ src/Components/Social/Social.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
+// import icons
+import { FaSpotify } from "react-icons/fa";
+
 const Social = () => {
   return (
     <div>
         
+        <a href="https://open.spotify.com/artist/5llELXjQfGt1bHjT9t6cqF" target="_blank" rel="noopener noreferrer"><FaSpotify className="socialicon"/></a>
     </div>
   )
 }
--- src/Pages/LandingPage/LandingPage.js
+++ src/Pages/LandingPage/LandingPage.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Logo from "../../img/grunyons.png";
 
+// import social media bar
+import Social from "../../Components/Social/Social"
+
 const LandingPage = () => {
   return (
     <div>
 
         <img src={Logo}></img>
         <h2 id="soon">New website coming soon.</h2>
+
+        <Social />
 
     </div>
   )
