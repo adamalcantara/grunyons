9edef9d3f6d21bc668f66273965d16d2f0b2caad
adamalcantara <alcantaradevelopment@gmail.com>
2022-06-05 23:09:24 -0400

    Adding initial code to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+// import pages
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       
-    </div>
+      <div className="App">
+          <Route exact path="/" component={LandingPage} />
+      </div>
+
+    </Router>
   );
 }
 
