7bb596d72ec5784663f302e95611cfa3c53e256e
adamalcantara <alcantaradevelopment@gmail.com>
2022-06-13 10:52:59 -0400

    Added pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Pages/Home/Home";
 
+
 function App() {
   return (
     <Router>
