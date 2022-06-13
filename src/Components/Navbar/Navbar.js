051b12f86bf08d97ee582f9794811ee3d6a76053
adamalcantara <alcantaradevelopment@gmail.com>
2022-06-12 23:43:12 -0400

    Initial navbar elements added

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -13,7 +13,11 @@ class Navbar extends Component {
         <div id="nav">
             <Link to="/" id="linkLogo"><img src={logo}></img></Link>
             <ul id="navigation">
-                
+                <li><Link to="/" className='menuItem'>Home</Link></li>
+                <li><Link to="/about" className='menuItem'>About</Link></li>
+                <li><Link to="/contact" className='menuItem'>Contact</Link></li>
+                <li><a href="https://m.facebook.com/TheGrunyons/?__tn__=C-R" target="_blank" rel="noopener noreferrer">Facebook</a></li>
+                <li><a href="https://open.spotify.com/artist/5llELXjQfGt1bHjT9t6cqF" target="_blank" rel="noopener noreferrer">Spotify</a></li>
             </ul>
         </div>
 
