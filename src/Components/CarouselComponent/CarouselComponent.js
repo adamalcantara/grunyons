9315852acefae5153dc5d98b0c3b450a09c5749b
adamalcantara <alcantaradevelopment@gmail.com>
2022-06-14 11:51:25 -0400

    Initial carousel applied

--- src/Components/CarouselComponent/CarouselComponent.js
+++ src/Components/CarouselComponent/CarouselComponent.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 
+//Carousel CSS
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "./CarouselComponent.css"
+
+import slider1 from "../../img/slider/slider1.jpg";
+import slider2 from "../../img/slider/slider2.jpg";
+import slider3 from "../../img/slider/slider3.jpg";
+import slider4 from "../../img/slider/slider4.jpg";
+
 const CarouselComponent = () => {
   return (
-    <div>CarouselComponent</div>
+    <div className="carousel-wrapper">
+        <Carousel>
+            <div>
+                <img src={slider1} alt="the grunyons at Spring Sing" />
+            </div>
+            <div>
+                <img src={slider2} alt="the grunyons at Spring Sing" />
+            </div>
+            <div>
+                <img src={slider3} alt="the grunyons at Spring Sing" />
+            </div>
+            <div>
+                <img src={slider4} alt="the grunyons at Spring Sing" />
+            </div>
+        </Carousel>
+    </div>
   )
 }
 
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+
+import CarouselComponent from '../../Components/CarouselComponent/CarouselComponent';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <CarouselComponent />
+    </div>
   )
 }
 
