e3ba79d0abcbbf2d7f2a8407cf820967cc8be903
adamalcantara <alcantaradevelopment@gmail.com>
2022-06-14 11:41:06 -0400

    Started to create the carousel

---
+++ src/Components/CarouselComponent/CarouselComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+
+const CarouselComponent = () => {
+  return (
+    <div>CarouselComponent</div>
+  )
+}
+
+export default CarouselComponent
