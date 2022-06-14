efcef0fe66fdb592327a2a7945bc5084e9d26374
adamalcantara <alcantaradevelopment@gmail.com>
2022-06-14 15:34:34 -0400

    About images added and styled

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -2,15 +2,19 @@ import React from 'react'
 
 import "./About.css";
 
+import Downtown from "../../img/about/downtown.jpg";
+import Trophy from "../../img/about/trophy.jpeg";
+import Ale from "../../img/about/ale.jpeg";
+
 const About = () => {
   return (
     <div>
       <p>
-      What do you call a men&#39;s vocal ensemble active in supporting and performing “vocal jazz”, a musical idiom involving the adaptation of instrumental jazz techniques to the rendition of vocal compositions? In southeast Michigan, you can call it the “Grunyons”.In 1949, eight local Yale graduates, yearning to continue the remarkable singing tradition of their university and especially the style of the Yale vocal ensemble, the “Whiffenpoofs”, banded together to form this group. The name “Grunyons” was chosen for reasons now lost to anyone&#39;s specific memory, but the ensemble soon expanded to a more versatile group of sixteen, drawing in men associated with other university musical experiences. The organization has continued since, offering its special brand of a cappella close harmony and entertainment to scores of audiences throughout southeast Michigan and beyond.
+      <img src={Downtown} id="downtownImg" alt="The Grunyons in downtown detroit"></img>What do you call a men&#39;s vocal ensemble active in supporting and performing “vocal jazz”, a musical idiom involving the adaptation of instrumental jazz techniques to the rendition of vocal compositions? In southeast Michigan, you can call it the “Grunyons”.In 1949, eight local Yale graduates, yearning to continue the remarkable singing tradition of their university and especially the style of the Yale vocal ensemble, the “Whiffenpoofs”, banded together to form this group. The name “Grunyons” was chosen for reasons now lost to anyone&#39;s specific memory, but the ensemble soon expanded to a more versatile group of sixteen, drawing in men associated with other university musical experiences. The organization has continued since, offering its special brand of a cappella close harmony and entertainment to scores of audiences throughout southeast Michigan and beyond.
 
       <div className='spacer'></div>
 
-      In 1966, the Grunyons began performing at a gathering of similar amateur vocal jazz ensembles called “Spring Sing“. Customarily held in various locations, “Spring Sing” is hosted by various groups in the organization known as the American A Cappella Alliance. “Spring Sing” has been hosted numerous times by The Grunyons, recently in 1997 in Dearborn, MI, in 2004 at Walt Disney World in FL, and in 2013 in Detroit, MI. Groups involved now represent about a dozen locations, from a far west as Denver Colorado.
+      <img src={Trophy} id="trophyImg" alt="The Grunyons with a trophy"></img>In 1966, the Grunyons began performing at a gathering of similar amateur vocal jazz ensembles called “Spring Sing“. Customarily held in various locations, “Spring Sing” is hosted by various groups in the organization known as the American A Cappella Alliance. “Spring Sing” has been hosted numerous times by The Grunyons, recently in 1997 in Dearborn, MI, in 2004 at Walt Disney World in FL, and in 2013 in Detroit, MI. Groups involved now represent about a dozen locations, from a far west as Denver Colorado.
 
       <div className='spacer'></div>
 
@@ -22,7 +26,7 @@ const About = () => {
 
       <div className='spacer'></div>
 
-      The Grunyons are now into their “middle ages”, alumni of some ten other colleges, an age range of almost exactly seventy years, several new vocal arrangements to update an extensive repertoire, and an enthusiasm for life and music which some say defies description. Perhaps it was best summed up in an introduction by a young student leader at a school assembly who was “happy to present the Grunyons, a bunch of middle-aged businessmen who really love to sing.”
+      <img src={Ale} id="aleImg" alt="The Grunyons with Pale Ale"></img>The Grunyons are now into their “middle ages”, alumni of some ten other colleges, an age range of almost exactly seventy years, several new vocal arrangements to update an extensive repertoire, and an enthusiasm for life and music which some say defies description. Perhaps it was best summed up in an introduction by a young student leader at a school assembly who was “happy to present the Grunyons, a bunch of middle-aged businessmen who really love to sing.”
 
       <div className='spacer'></div>
 
