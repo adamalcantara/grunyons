7d72958f402869543f56b5a08ffe5d049b3be85e
adamalcantara <alcantaradevelopment@gmail.com>
2022-06-15 10:24:34 -0400

    Footer started

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div>
+        <p>&copy; 2022. Website designed and maintained by <a href='https://alcantaradevelopment.com/' target="_blank" rel="noopener noreferrer" id="alcantaraDev">Alcantara Development</a></p>
+    </div>
+  )
+}
