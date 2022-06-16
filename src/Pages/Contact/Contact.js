1b2b7dd50c627972c53b77af97aa1498233b5133
adamalcantara <alcantaradevelopment@gmail.com>
2022-06-16 10:28:30 -0400

    Contact page styled

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import "./Contact.css";
+
 import ContactUs from "../../Components/ContactUs/ContactUs";
+import ContactImg from "../../img/contact.jpeg";
 
 const Contact = () => {
   return (
-    <div>
+    <div id="contactPage">
+      <p id="useForm">Please use the form below to contact us!</p>
       <ContactUs />
     </div>
   )
