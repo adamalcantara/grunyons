5324896775248d8253cb2d181c605fbe2619b12d
adamalcantara <alcantaradevelopment@gmail.com>
2022-09-08 23:35:13 -0400

    Removing unused code

--- src/Components/Social/Social.js
+++ src/Components/Social/Social.js
@@ -4,7 +4,6 @@ import "./Social.css";
 // import icons
 import { FaSpotify } from "react-icons/fa";
 import { FaFacebookF } from "react-icons/fa";
-import { FaItunes } from "react-icons/fa";
 
 const Social = () => {
   return (
