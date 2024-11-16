8dba51b09a630744a05e16f90c6e0483cb03ad27
adamalcantara <alcantaradevelopment@gmail.com>
2024-11-16 14:29:16 -0500

    Added media queries

--- src/Components/Upcoming/Upcoming.js
+++ src/Components/Upcoming/Upcoming.js
@@ -15,7 +15,7 @@ const Upcoming = () => {
             <tr>
                 <td>12&#47;9&#47;24</td>
                 <td>Grosse Pointe United Methodist Church</td>
-                <td><a href='https://www.eventbrite.com/e/christmas-on-lakeshore-2024-an-evening-of-a-cappella-tickets-1073998060109?utm-campaign=social&utm-content=attendeeshare&utm-medium=discovery&utm-term=listing&utm-source=cp&aff=ebdsshcopyurl'>Buy Tickets</a></td>
+                <td className='performanceButton'><a href='https://www.eventbrite.com/e/christmas-on-lakeshore-2024-an-evening-of-a-cappella-tickets-1073998060109?utm-campaign=social&utm-content=attendeeshare&utm-medium=discovery&utm-term=listing&utm-source=cp&aff=ebdsshcopyurl'>Buy Tickets</a></td>
             </tr>
         </table>
     </div>
