b0ca40076869e031946dd677816d1be3ca246a8b
adamalcantara <alcantaradevelopment@gmail.com>
2024-11-16 14:19:26 -0500

    Added skeleton code for performances

--- src/Components/Upcoming/Upcoming.js
+++ src/Components/Upcoming/Upcoming.js
@@ -3,7 +3,22 @@ import './Upcoming.css';
 
 const Upcoming = () => {
   return (
-    <div>Upcoming</div>
+    <div>
+        <p>Upcoming Performances:</p>
+
+        <table>
+            <tr>
+                <td>12&#47;6&#47;24</td>
+                <td>Rackham Auditorium, Ann Arbor</td>
+                <td><a>Buy Tickets</a></td>
+            </tr>
+            <tr>
+                <td>12&#47;9&#47;24</td>
+                <td>Grosse Pointe United Methodist Church</td>
+                <td><a>Buy Tickets</a></td>
+            </tr>
+        </table>
+    </div>
   )
 }
 
