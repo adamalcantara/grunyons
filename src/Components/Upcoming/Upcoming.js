740c989f23c4f1b8266e82da92e98e30d8ddf4cc
adamalcantara <alcantaradevelopment@gmail.com>
2024-11-19 23:40:06 -0500

    Updated the link

--- src/Components/Upcoming/Upcoming.js
+++ src/Components/Upcoming/Upcoming.js
@@ -10,7 +10,7 @@ const Upcoming = () => {
             <tr>
                 <td>12&#47;6&#47;24</td>
                 <td>Rackham Auditorium, Ann Arbor</td>
-                <td><a href='https://www.eventbrite.com/e/christmas-on-lakeshore-2024-an-evening-of-a-cappella-tickets-1073998060109?utm-campaign=social&utm-content=attendeeshare&utm-medium=discovery&utm-term=listing&utm-source=cp&aff=ebdsshcopyurl' target="_blank" className='performanceButton'>Buy Tickets</a></td>
+                <td><a href='https://www.eventbrite.com/e/the-friars-69th-annual-study-break-concert-tickets-1081484873369' target="_blank" className='performanceButton'>Buy Tickets</a></td>
             </tr>
             <tr>
                 <td>12&#47;9&#47;24</td>
