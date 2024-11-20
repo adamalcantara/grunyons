af9e5049ee9223bc06f32f9bd96dc5d208f7c3e1
adamalcantara <alcantaradevelopment@gmail.com>
2024-11-19 23:39:16 -0500

    Removed excess code

--- src/Components/Upcoming/Upcoming.js
+++ src/Components/Upcoming/Upcoming.js
@@ -7,12 +7,12 @@ const Upcoming = () => {
         <p id='performanceHeader'>Upcoming Performances:</p>
 
         <table id='performanceTable'>
-            <tr className='performanceRow'>
+            <tr>
                 <td>12&#47;6&#47;24</td>
                 <td>Rackham Auditorium, Ann Arbor</td>
                 <td><a href='https://www.eventbrite.com/e/christmas-on-lakeshore-2024-an-evening-of-a-cappella-tickets-1073998060109?utm-campaign=social&utm-content=attendeeshare&utm-medium=discovery&utm-term=listing&utm-source=cp&aff=ebdsshcopyurl' target="_blank" className='performanceButton'>Buy Tickets</a></td>
             </tr>
-            <tr className='performanceRow'>
+            <tr>
                 <td>12&#47;9&#47;24</td>
                 <td>Grosse Pointe United Methodist Church</td>
                 <td><a href='https://www.eventbrite.com/e/christmas-on-lakeshore-2024-an-evening-of-a-cappella-tickets-1073998060109?utm-campaign=social&utm-content=attendeeshare&utm-medium=discovery&utm-term=listing&utm-source=cp&aff=ebdsshcopyurl' target="_blank" className='performanceButton'>Buy Tickets</a></td>
